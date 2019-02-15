556e1113d7aa4825543c62cf9e7079807b1f3c06
BUAAHugeGun <1019658949@qq.com>
2019-02-15 20:42:46 +0800

    modify data_maker and generate_data_list

--- code/CRRN/data_maker.py
+++ code/CRRN/data_maker.py
@@ -7,49 +7,56 @@ import sys
 sys.path.append(
     'E:\CCTV\CRRN\coco\PythonAPI')
 
+dataDir = 'E:/CCTV/CRRN/coco'
+dataType = 'val2017'
+annFile = '{}/annotations/instances_{}.json'.format(dataDir, dataType)
+coco = COCO(annFile)
+
+
+def combine(image_a, image_b):
+    x = random.uniform(0.8, 1)
+    y = random.uniform(0.1, 0.5)
+    image_ret = cv2.addWeighted(image_a, x, image_b, y, 0)
+    return image_ret, image_a
+
+
+def translet_ran(image):
+    r, c, p = image.shape
+    mat = np.float64([[1, 0, random.uniform(-10, 10)],
+                      [0, 1, random.uniform(-10, 10)]])
+    return cv2.warpAffine(image, mat, (r, c))
+
+
+def translet(image, x, y):
+    r, c, p = image.shape
+    mat = np.float64([[1, 0, x], [0, 1, y]])
+    return cv2.warpAffine(image, mat, (r, c))
+
+
+def rotate_ran(image):
+    r, c, p = image.shape
+    x, y = random.uniform(0, r), random.uniform(0, c)
+    mat = cv2.getRotationMatrix2D(
+        (x, y), random.uniform(-20, 20), random.uniform(0.9, 1.1))
+    cv2.warpAffine(image, mat, (r, c))
+
+
+def rotate(image, x, y, angle, rate):
+    r, c, p = image.shape
+    mat = cv2.getRotationMatrix2D((x, y), angle, rate)
+    cv2.warpAffine(image, mat, (r, c))
+
+
+def cut(image_a, image_b):
+    s = image_a.shape
+    t = image_b.shape
+    image_a = image_a[0:min(s[0], t[0]), 0:min(s[1], t[1])]
+    image_b = image_b[0:min(s[0], t[0]), 0:min(s[1], t[1])]
+    return image_a, image_b
+
 
 def main():
-    dataDir = 'E:/CCTV/CRRN/coco'
-    dataType = 'val2017'
-    annFile = '{}/annotations/instances_{}.json'.format(dataDir, dataType)
-    coco = COCO(annFile)
-
-    def combine(image_a, image_b):
-        x = random.uniform(0.8, 1)
-        y = random.uniform(0.1, 0.5)
-        image_ret = cv2.addWeighted(image_a, x, image_b, y, 0)
-        return image_ret, image_a
-
-    def translet_ran(image):
-        r, c, p = image.shape
-        mat = np.float64([[1, 0, random.uniform(-10, 10)],
-                          [0, 1, random.uniform(-10, 10)]])
-        return cv2.warpAffine(image, mat, (r, c))
-
-    def translet(image, x, y):
-        r, c, p = image.shape
-        mat = np.float64([[1, 0, x], [0, 1, y]])
-        return cv2.warpAffine(image, mat, (r, c))
-
-    def rotate_ran(image):
-        r, c, p = image.shape
-        x, y = random.uniform(0, r), random.uniform(0, c)
-        mat = cv2.getRotationMatrix2D(
-            (x, y), random.uniform(-20, 20), random.uniform(0.9, 1.1))
-        cv2.warpAffine(image, mat, (r, c))
-
-    def rotate(image, x, y, angle, rate):
-        r, c, p = image.shape
-        mat = cv2.getRotationMatrix2D((x, y), angle, rate)
-        cv2.warpAffine(image, mat, (r, c))
-
-    def cut(image_a, image_b):
-        s = image_a.shape
-        t = image_b.shape
-        image_a = image_a[0:min(s[0], t[0]), 0:min(s[1], t[1])]
-        image_b = image_b[0:min(s[0], t[0]), 0:min(s[1], t[1])]
-        return image_a, image_b
-
+    '''
     ids1 = coco.getCatIds(catNms=['indoor'])
     imgid1 = coco.getImgIds(catIds=ids1)
 
@@ -70,6 +77,19 @@ def main():
 
         cv2.imwrite('E:/CCTV/CRRN/data/input{}.jpg'.format(i), out1)
         cv2.imwrite('E:/CCTV/CRRN/data/truth{}.jpg'.format(i), out1)
+    '''
+    f = open('train.txt')
+    for line in f:
+        st = line.strip()
+        le = len(st)
+        st1 = st[:le // 2]
+        st2 = st[le // 2 + 1:]
+        img1 = cv2.imread(st1)
+        img2 = cv2.imread(st2)
+        img1, img2 = cut(img1, img2)
+        img = combine(img1, img2)
+        cv2.imshow('x', img[0])
+        cv2.waitKey(0)
 
 
 if __name__ == "__main__":
--- code/CRRN/generate_data_list.py
+++ code/CRRN/generate_data_list.py
@@ -1,24 +1,43 @@
+import cv2
 import os
 import numpy as np
+
 
 def generate(img_path, output_path, generate_per_img=5):
     img_list = [item for item in os.listdir(img_path)]
     length = len(img_list)
     items = []
     for i in range(length):
+        print(i)
+        tot = 0
+        if len(items) > 5000:
+            break
         for j in range(generate_per_img):
             content = img_path + img_list[i] + ','
-            random_val = np.random.randint(0, length-1)
-            content += img_path + img_list[random_val] + '\n'
-            items.append(content)
+            random_val = np.random.randint(0, length - 1)
+            img1 = cv2.imread(img_path + img_list[i])
+            img2 = cv2.imread(img_path + img_list[random_val])
+            a = np.sum(img1)
+            b = np.sum(img2)
+            # 两张图片亮度差距不要太大。（0.6,0.8）的话在本地跑100张要半分钟
+            if a / b < 0.8 and a / b > 0.6:
+                content += img_path + img_list[random_val] + '\n'
+                items.append(content)
+            else:
+                generate_per_img += 1
+                tot += 1
+            # 防止死循环
+            if tot > 5: break
     print(len(items))
     with open(output_path, 'w') as f:
         f.writelines(items)
 
-            
+
 def main():
-    coco_val_path = '/mnt/lustre/share/DSK/datasets/mscoco2017/val2017/'
-    coco_test_path = '/mnt/lustre/share/DSK/datasets/mscoco2017/test2017/'
+    # coco_val_path = '/mnt/lustre/share/DSK/datasets/mscoco2017/val2017/'
+    # coco_test_path = '/mnt/lustre/share/DSK/datasets/mscoco2017/test2017/'
+    coco_test_path = '../../../CRRN/coco/images/test2017/'
+    coco_val_path = '../../../CRRN/coco/images/val2017/'
     output_train = './train.txt'
     output_test = './test.txt'
     generate(coco_test_path, output_train)
