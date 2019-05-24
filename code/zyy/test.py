22602282aaecc153c31d8b21c206ca45485a9c03
HugeGun <1019658949@qq.com>
2019-05-24 17:44:35 +0800

    remove for()

--- code/zyy/test.py
+++ code/zyy/test.py
@@ -1,40 +1,47 @@
+from typing import Any, Union
+
 import torch
 from torch import nn
 
 
-def myconv(mat, inchannel, outchannel, weight, kernel=3, stride=1, padding=1, paddingmode="reflect"):
-	if paddingmode == "reflect":
-		temp = nn.ReflectionPad2d(padding)
-		mat = temp(mat)
+def myconv(mat, weight, kernel=3, stride=1, padding=1, paddingmode="reflect"):
+    if paddingmode == "reflect":
+        temp = nn.ReflectionPad2d(padding)
+        mat = temp(mat)
 
-	groups, channel, H, W = mat.shape
-	newH, newW = (H + 1 - kernel) // stride, (W + 1 - kernel) // stride
-	ret = torch.randn(groups, outchannel, newH, newW)
-	for group in range(0, groups):
-		for k in range(0, outchannel):
-			for i in range(0, newH):
-				for j in range(0, newW):
-					temp = mat[group:group + 1, :, i:i + kernel, j:j + kernel]
-					# print(temp.shape)
-					kernelmat = weight[k:k + 1, :, :]
-					ret[group][k][i][j] = torch.sum(temp * kernelmat)
-	return ret.float()
+    groups, inchannel, H, W = mat.shape
+    outchannel, inchannel, kernel, kernel = weight.shape
+    newH, newW = (H + 1 - kernel) // stride, (W + 1 - kernel) // stride
+
+    weight = weight.reshape(outchannel, inchannel, 1, kernel * kernel)
+    weight = weight.expand(-1, -1, newH * newW, -1)
+
+    change = nn.Unfold((newH, newW), padding=0)
+    mat = change(mat)
+    mat = mat.reshape(groups, inchannel, -1, kernel * kernel)
+    mat = mat.expand(outchannel, -1, -1, -1)
+
+    ret = weight * mat
+    ret = torch.sum(ret, 3)
+    ret = torch.sum(ret, 1)
+    ret = ret.reshape(-1, newH, newW)
+    return ret
 
 
 if __name__ == '__main__':
-	x = torch.randn(1, 3, 3, 4)
-	temp = nn.ReflectionPad2d(1)
-	y = temp(x)
+    x = torch.randn(1, 3, 3, 4)
+    temp = nn.ReflectionPad2d(1)
+    y = temp(x)
 
-	kernel = torch.randn(4, 3, 3, 3)
-	ret = nn.functional.conv2d(y, kernel, padding=0)
+    kernel = torch.randn(4, 3, 3, 3)
+    ret = nn.functional.conv2d(y, kernel, padding=0)
 
-	# print(ret.shape)
-	print(ret)
+    # print(ret.shape)
+    print(ret)
 
-	ret2 = myconv(x, 3, 4, kernel, 3, 1, 1, "reflect")
+    ret2 = myconv(x, kernel, 3, 1, 1, "reflect")
 
-	# print(ret.shape)
-	print(ret2)
+    # print(ret.shape)
+    print(ret2)
 
-	print(torch.sum(ret-ret2))
+    print(torch.sum(ret - ret2))
