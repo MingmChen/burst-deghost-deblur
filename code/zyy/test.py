968e3ba4a19c74bee01c265a1369b79262b65e3e
HugeGun <1019658949@qq.com>
2019-05-24 19:12:38 +0800

    group

--- code/zyy/test.py
+++ code/zyy/test.py
@@ -13,23 +13,24 @@ def myconv(mat, weight, kernel=3, stride=1, padding=1, paddingmode="reflect"):
     outchannel, inchannel, kernel, kernel = weight.shape
     newH, newW = (H + 1 - kernel) // stride, (W + 1 - kernel) // stride
 
-    weight = weight.reshape(outchannel, inchannel, 1, kernel * kernel)
-    weight = weight.expand(-1, -1, newH * newW, -1)
+    weight = weight.reshape(1, outchannel, inchannel, 1, kernel * kernel)
+    weight = weight.expand(groups, -1, -1, newH * newW, -1)
 
     change = nn.Unfold((newH, newW), padding=0)
     mat = change(mat)
-    mat = mat.reshape(groups, inchannel, -1, kernel * kernel)
-    mat = mat.expand(outchannel, -1, -1, -1)
+    mat = mat.reshape(groups, 1, inchannel, -1, kernel * kernel)
+    mat = mat.expand(-1, outchannel, -1, -1, -1)
+
 
     ret = weight * mat
-    ret = torch.sum(ret, 3)
-    ret = torch.sum(ret, 1)
-    ret = ret.reshape(-1, newH, newW)
+    ret = torch.sum(ret, 4)
+    ret = torch.sum(ret, 2)
+    ret = ret.reshape(groups, -1, newH, newW)
     return ret
 
 
 if __name__ == '__main__':
-    x = torch.randn(1, 3, 3, 4)
+    x = torch.randn(2, 3, 3, 4)
     temp = nn.ReflectionPad2d(1)
     y = temp(x)
 
