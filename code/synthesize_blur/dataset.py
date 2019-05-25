0eb0cd5bf92452db6647a29207ab0304e1c17d0f
PaParaZz1 <niuyazhe@buaa.edu.cn>
2019-05-25 22:24:07 +0800

    add naive blur dataset

--- code/synthesize_blur/dataset.py
+++ code/synthesize_blur/dataset.py
@@ -1,25 +1,33 @@
-#import os
-import torch
+import os
+from PIL import Image
 from torch.utils.data import Dataset
-#from torchvision import transforms, utils
 
 
 class BlurDataset(Dataset):
-    def __init__(self, transform = None):
+    def __init__(self, root=None, train=True, transform=None):
+        assert(root is not None)
+        assert(transform is not None)
         self.transform = transform
-
+        if train:
+            self.data_path = os.path.join(root, 'train.txt')
+        else:
+            self.data_path = os.path.join(root, 'test.txt')
+        with open(self.data_path, 'r') as f:
+            self.data_list = f.readlines()
 
     def __len__(self):
-        return  1024
+        return len(self.data_list)
 
+    def open_image(self, path):
+        return Image.Open(path).convert('RGB')
 
-    def __getitem__(self, item):
-        sample = {'input1': torch.rand(3,256,256),
-                  'input2': torch.rand(3,256,256),
-                  'output': torch.rand(3,256,256)}
-        return sample
+    def __getitem__(self, idx):
+        item = self.data_list[idx]
+        split_item = item[:-1].split('\t')
+        data = []
+        for path in split_item:
+            img = self.open_image(path)
+            img = self.transform(img)
+            data.append(img)
 
-
-
-
-
+        return data
